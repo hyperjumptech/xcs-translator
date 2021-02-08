8e347d9d0301c2e79017a608be08630b5a7d6cfe
Armedi <armedi@hyperjump.tech>
2021-02-08 15:59:09 +0700

    create app skeleton with express.js

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+interface Config {
+  env: string
+  port: string
+}
+
+export const cfg: Config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || '8080',
+}
