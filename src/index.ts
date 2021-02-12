07f2e38a5711d9f8c03caefd1fd7451f2b757b6f
Hari Cahya Nugraha <haricnugraha@gmail.com>
2021-02-12 21:49:15 +0700

    Feat: Add database connection into health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,39 @@ import errorHandler from './internal/middleware/error-handler'
 import uploads from './service/uploads'
 
 import { cfg } from './config'
+import { endPool, getConnection } from './database/mariadb'
+import { AppError, commonHTTPErrors } from './internal/app-error'
 
 const app = express()
 const port = cfg.port
 
 app.use(express.static('public'))
 
-app.get('/health', (_, res) => {
-  res.status(200).json({ alive: true })
+app.get('/health', async (_, res, next) => {
+  // TODO: Remove hardcode
+  let connAntigen, connPCR
+  try {
+    ;[connAntigen, connPCR] = await Promise.all([
+      getConnection('antigen'),
+      getConnection('pcr'),
+    ])
+    await Promise.all([
+      connAntigen.query('SELECT 1'),
+      connPCR.query('SELECT 1'),
+    ])
+
+    res.status(200).json({ alive: true, is_all_db_connected: true })
+  } catch (error) {
+    const err = new AppError(
+      commonHTTPErrors.unprocessableEntity,
+      'Database is not connected',
+      false,
+    )
+    next(err)
+  } finally {
+    if (connAntigen) return connAntigen.release()
+    if (connPCR) return connPCR.release()
+  }
 })
 app.use(uploads)
 app.use(errorHandler())
@@ -28,6 +53,7 @@ let server: http.Server
 const stopServer = async () => {
   logger.info('  Shutting down the server . . .')
   if (server.listening) {
+    endPool()
     logger.close()
     server.close()
   }
