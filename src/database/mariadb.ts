ce026ece678cf9a96ae05d159476eb8b670baa0c
Hari Cahya Nugraha <haricnugraha@gmail.com>
2021-02-13 16:14:04 +0700

    Fix: Connection pool by file type

--- src/database/mariadb.ts
+++ src/database/mariadb.ts
@@ -24,7 +24,7 @@ const db2Pool = mariadb.createPool({
 })
 
 export async function getConnection(type: string) {
-  if (type === 'db1') {
+  if (type === 'antigen') {
     return await db1Pool.getConnection()
   }
   return await db2Pool.getConnection()
