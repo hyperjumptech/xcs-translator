573525d66423b32ee9b779d4fae6435497f4ee1f
Hari Cahya Nugraha <haricnugraha@gmail.com>
2021-02-14 22:30:02 +0700

    Fix: Connection pool

--- src/database/mariadb.ts
+++ src/database/mariadb.ts
@@ -2,37 +2,27 @@ import mariadb from 'mariadb'
 import { cfg } from '../config'
 
 const { db } = cfg
-let pool: Map<String, mariadb.Pool> = new Map()
+const pools = db.map(database => {
+  const pool = mariadb.createPool({
+    host: database.host,
+    port: database.port,
+    database: database.database,
+    user: database.user,
+    password: database.password,
+    connectionLimit: database.connectionLimit,
+  })
+
+  return { databaseID: database.id, pool }
+})
 
 // Connection pools reuse connections between invocations,
 // and handle dropped or expired connections automatically.
 export async function getConnection(type: string) {
-  let pol = pool.get(type)
-  if (pol) {
-    return await pol.getConnection()
-  }
-
-  for (let con of db) {
-    if (type === con.id) {
-      pol = mariadb.createPool({
-        host: con.host,
-        port: con.port,
-        database: con.database,
-        user: con.user,
-        password: con.password,
-        connectionLimit: con.connectionLimit,
-      })
-
-      pool.set(type, pol)
-      break
-    }
-  }
-
-  return await pol?.getConnection()
+  return await pools
+    .find(pool => pool.databaseID === type)
+    ?.pool.getConnection()
 }
 
 export async function endPool() {
-  pool.forEach(async pol => {
-    await pol.end()
-  })
+  await Promise.all(pools.map(pool => pool.pool.end()))
 }
