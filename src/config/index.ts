a01595806447b3030aab1217eea00bd6448fedf3
Hari Cahya Nugraha <haricnugraha@gmail.com>
2021-02-14 22:29:48 +0700

    Fix: typo .env key

--- src/config/index.ts
+++ src/config/index.ts
@@ -26,7 +26,7 @@ export const cfg: Config = {
       id: process.env.DB_ID || 'data1',
       host: process.env.DB_HOST || 'localhost',
       port: parseInt(String(process.env.DB_PORT), 10) || 3306,
-      database: process.env.DBNAME,
+      database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       connectionLimit: 5,
