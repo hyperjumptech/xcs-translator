2748bfdab353cf92b27299228ea3feb16a6db007
Hari Cahya Nugraha <haricnugraha@gmail.com>
2021-02-15 08:56:04 +0700

    Feat: Error Tracing (#31)
    * Feat: Add failed directory
    
    * Feat: Move failed file to failed directory
    
    * Feat: Add file based log for easier tracing error message
    
    * Feat: Add excel and json directory in failed directory

--- src/internal/logger.ts
+++ src/internal/logger.ts
@@ -1,15 +1,48 @@
-import winston from 'winston'
+import { transports, format, LoggerOptions, createLogger } from 'winston'
 import { cfg } from '../config'
+const { combine, timestamp, colorize, simple, json, printf } = format
 
-const transports = [
-  new winston.transports.Console({
-    level: cfg.env === 'production' ? 'info' : 'debug',
+const productionTransport = [
+  new transports.Console({
+    level: 'info',
+    format: combine(
+      timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss',
+      }),
+      json(),
+    ),
+  }),
+  new transports.File({
+    filename: 'error.log',
+    level: 'error',
+    format: combine(
+      timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss',
+      }),
+      simple(),
+      printf(msg => `${msg.timestamp} - ${msg.level}: ${msg.message}`),
+    ),
   }),
 ]
-
-const options: winston.LoggerOptions = {
-  transports,
+const developmentTransport = [
+  new transports.Console({
+    level: 'debug',
+    format: combine(
+      timestamp(),
+      simple(),
+      printf(msg =>
+        colorize().colorize(
+          msg.level,
+          `${msg.timestamp} - ${msg.level}: ${msg.message}`,
+        ),
+      ),
+    ),
+  }),
+]
+const options: LoggerOptions = {
+  transports:
+    cfg.env === 'production' ? productionTransport : developmentTransport,
 }
 
-export const logger = winston.createLogger(options)
+export const logger = createLogger(options)
 logger.debug('  Logging initialized at debug level')
